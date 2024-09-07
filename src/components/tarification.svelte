efb73b30b0c2c01d5b1b2ee3a2aa96988c6ade95
alphacentorus <alphacentorus@gmail.com>
2024-09-07 18:37:04 +0200

    style(presentation) : remove bold style

--- src/components/tarification.svelte
+++ src/components/tarification.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <div id="tarifs" class="flex flex-col grow shrink-0 lg:order-3 lg:basis-[27rem] p-8  lg:rounded-3xl lg:shadow-2xl bg-[#f7f7f3]">
-    <h2 class="text-2xl mb-4 text-dark-green">Tarification</h2>
+    <h2 class="text-2xl mb-4 text-dark-green">Tarifs</h2>
     <ul class="shrink-0 grow text-lg">
-        <li class="mb-2"><span class="font-bold">Consultation/suivi en psychologie ou TCC: </span><span>70€</span></li>
-        <li class="mb-2"><span class="font-bold">Sophrologie: </span><span>60€</span></li>
-        <li class="mb-2"><span class="font-bold">Sophrologie enfant - de 8 ans: </span><span>50€</span></li>
+        <li class="mb-2"><span >Consultation/suivi en psychologie ou TCC: </span><span>70€</span></li>
+        <li class="mb-2"><span >Sophrologie: </span><span>60€</span></li>
+        <li class="mb-2"><span >Sophrologie enfant - de 8 ans: </span><span>50€</span></li>
     </ul>
     <a class="flex items-center gap-2 p-2 rounded-md border mt-6 transition-all bg-blue-500 text-white border-blue-500  w-max hover:bg-blue-600 uppercase
      " href="https://www.doctolib.fr/psychologue/montgeron/joanna-wolna" target="_blank">Prendre rendez-vous<SvgDoctolib /> </a>
